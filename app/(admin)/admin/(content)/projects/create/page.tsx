81b1b41ddc01f721f5367cac61a0b7dff75acbe3
Thao1904 <letthao17@gmail.com>
2026-08-19 22:35:27 +0700

    create form project

--- app/(admin)/admin/(content)/projects/create/page.tsx
+++ app/(admin)/admin/(content)/projects/create/page.tsx
@@ -8,23 +8,38 @@ const actionButtons = [
   <AdminButton
     label="Preview"
     className="border pl-2 pr-6 py-1 text-secondary font-semibold text-sm hover:cursor-pointer"
-    action={() => {}}
+    action={() => { }}
   />,
   <AdminButton
     label="Save Draft"
     className="border pl-2 pr-6 py-1 text-secondary font-semibold text-sm hover:cursor-pointer"
-    action={() => {}}
+    action={() => { }}
   />,
   <AdminButton
     label="Publish"
     className="border bg-primary pl-2 pr-6 py-1 text-secondary font-semibold text-sm hover:cursor-pointer"
-    action={() => {}}
+    action={() => { }}
   />,
 ];
 
 export default function CreateProjectPage() {
   const { setActionButtons, setCurrentHeader } = useAdmin();
   const [categories, setCategories] = useState<any[]>([]);
+  const [form, setForm] = useState(
+    {
+      title: "",
+      short_description: "",
+      content: "",
+      thumbnail: "",
+      password: "",
+      ref_link: "",
+      categories: [],
+      is_publish: false,
+      is_private: false
+    },
+
+
+  );
 
   const fetchCategories = async () => {
     const res = await fetch("/api/categories");
@@ -38,6 +53,7 @@ export default function CreateProjectPage() {
     setActionButtons(actionButtons);
     fetchCategories();
   }, []);
+
 
   return (
     <div className="w-full bg-[#F5F5F5] col-span-4">
@@ -57,6 +73,62 @@ export default function CreateProjectPage() {
           <></>
         )}
       </div>
+      <input
+        placeholder="Title"
+        value={form.title}
+        onChange={(e) =>
+          setForm({ ...form, title: e.target.value })
+        }
+      />
+      <input
+        placeholder="Short description"
+        value={form.short_description}
+        onChange={(e) =>
+          setForm({ ...form, short_description: e.target.value })
+        }
+      />
+      <input
+        placeholder="Content"
+        value={form.content}
+        onChange={(e) =>
+          setForm({ ...form, content: e.target.value })
+        }
+      />
+      <input
+        placeholder="Thumbnail"
+        value={form.thumbnail}
+        onChange={(e) =>
+          setForm({ ...form, thumbnail: e.target.value })
+        }
+      />
+      <input
+        placeholder="Password"
+        value={form.password}
+        onChange={(e) =>
+          setForm({ ...form, password: e.target.value })
+        }
+      />
+      <input
+        placeholder="Project link"
+        value={form.ref_link}
+        onChange={(e) =>
+          setForm({ ...form, ref_link: e.target.value })
+        }
+      />
+      
+
     </div>
+
+
   );
 }
+// title: {required: true, type: String},
+//     short_description: {required: true, type: String},
+//     content: {required: true, type: String}, //html => covert string => save to db
+//     thumbnail: {required: true, type: String}, //upload cloud -> get public_url -> thumbnail = public_url => save to db
+//     password: {required: false, type: String},
+//     ref_link: {required: false, type: String},
+//     categories: {required: true, type: [Schema.Types.ObjectId], ref: "Category"},
+//     viewed: {required: false, type: Number, default: 0},
+//     is_publish: {required: false, type: Boolean, default: true},
+//     is_private: {required: false, type: Boolean, default: false}
